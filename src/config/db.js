9733ffd8b58a22ba40cb829635b581bccf8bd560
ningyongheng <ningyongheng@jeejio.com>
2025-06-24 11:16:54 +0800

    feat: 恢复数据库的查询修改：使用线上海外的数据库内容

--- src/config/db.js
+++ src/config/db.js
@@ -6,16 +6,11 @@ import mysql from "serverless-mysql";
 // database: "my_next_db",
 export const pool = mysql({
   config: {
-    // host: "rm-0xi8e03unv1008it6.mysql.rds.aliyuncs.com", // 改为远程地址
-    // user: "nodejs", // 改为远程数据库账号
-    // password: "Ec^jL33KBq6a", // 改为远程数据库密码
-    // port: 3306, // MySQL 默认端口
-    // database: "jeejio_next_db", // 数据库名称
-    host: "localhost",
-    user: "henry",
-    password: "nyh123",
-    port: 3306,
-    database: "my_next_db",
+    host: "rm-0xi8e03unv1008it6.mysql.rds.aliyuncs.com", // 改为远程地址
+    user: "nodejs", // 改为远程数据库账号
+    password: "Ec^jL33KBq6a", // 改为远程数据库密码
+    port: 3306, // MySQL 默认端口
+    database: "jeejio_next_db", // 数据库名称
     // 增加一个数据库的初始化table表的实现
   },
 });
